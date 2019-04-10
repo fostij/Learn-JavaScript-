74ba08fc086602c7e7141b04e41b08e80d4dca14
Oleg Fostij <olegfostij@gmail.com>
2019-04-10 17:10:58 +0300

    Це шостий комміт

--- js/script.js
+++ js/script.js
@@ -286,3 +286,35 @@ range.oninput = function(){
 };
 **/
 
+/** if else switch;
+  let num = 23;
+
+if(num == 24){
+    console.log('Верно');
+}else if(num > 100){
+    console.log('менше');
+}else if(num <= 23) {
+    console.log('==');
+}
+else {
+console.log('Значення не співпадають');
+}
+
+switch(num){
+    case num == 24:
+    console.log('Верно');
+    break;
+    case num > 100:
+    console.log('менше');
+    break;
+    case 23:
+    console.log('==');
+    break;
+    default:
+    console.log('Значення не співпадають');
+    break;
+
+*/
+
+/** Цикли*/
+
