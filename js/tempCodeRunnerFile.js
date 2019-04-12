048b8dc51154ec38ae64c3ceb49f958b47f42a4b
Oleg Fostij <olegfostij@gmail.com>
2019-04-12 11:57:53 +0300

    Function

--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,27 +1,9 @@
-let num = 23;
-
-if(num == 24){
-    console.log('Верно');
-}else if(num > 100){
-    console.log('менше');
-}else if(num <= 23) {
-    console.log('==');
-}
-else {
-console.log('Значення не співпадають');
-}
-
-switch(num){
-    case num == 24:
-    console.log('Верно');
-    break;
-    case num > 100:
-    console.log('менше');
-    break;
-    case 23:
-    console.log('==');
-    break;
-    default:
-    console.log('Значення не співпадають');
-    break;
+for(let i = 0; i <= 8; i++){
+    if(i == 8){
+        break;
+    }
+    if(i == 7){
+        continue;
+    }
+ console.log(i);
 }
